f389cc66815a9cf297a70f32411be6d72cedbef2
aklciot <39285415+aklciot@users.noreply.github.com>
2019-06-04 17:05:14 +1200

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Node, NodeUser, Profile
-
-admin.site.register(Node)
-admin.site.register(NodeUser)
-admin.site.register(Profile)
