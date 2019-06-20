19c52ccdf57f43ec0a4b07676b2f8ade3fb27c21
aklciot <39285415+aklciot@users.noreply.github.com>
2019-06-20 16:01:13 +1200

    Add files via upload

--- aklc/monitor/views.py
+++ aklc/monitor/views.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Node, NodeUser
+from .forms import NodeDetailForm
 
 class IndexView(generic.ListView):
     template_name = "monitor/index.html"
@@ -22,16 +25,31 @@ def index(request):
     context = {'nodeList': nodeList}
     return render(request, 'monitor/index.html', context)
 
+@login_required
 def nodeDetail(request, node_ref):
     node = Node.objects.get(id = node_ref)
-    aPpl = NodeUser.objects.filter(nodeID = node)
-    context = {'node': node, 'user': request.user, 'people': aPpl}
+    context = {'node': node, 'user': request.user}
     return render(request, 'monitor/nodeDetail.html', context)
 
+@login_required
 def nodeUpdate(request, node_ref):
+    node = get_object_or_404(Node, pk=node_ref)
+    if request.method == 'POST':
+        nf = NodeDetailForm(request.POST, instance=node)
+        if nf.is_valid():
+            nf.save()
+            return HttpResponseRedirect(reverse('monitor:nodeDetail', args=[node.id]))
+     # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NodeDetailForm(instance=node)
+    context = {'form': form, 'node': node}
+    return render(request, 'monitor/nodeUpdate.html', context)
+
+@login_required
+def nodeModNotify(request, node_ref):
     node = Node.objects.get(id = node_ref)
     context = {'node': node, 'user': request.user}
-    return render(request, 'monitor/nodeUpdate.html', context)
+    return render(request, 'monitor/nodeModNotify.html', context)
 
 def tb1(request, node_ref):
     node = Node.objects.get(id = node_ref)
